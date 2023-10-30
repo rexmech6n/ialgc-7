5567dd184551c4274deddd692c0c961e12e3f208
Cem Baykara <cm0bykr@gmail.com>
2023-10-30 22:13:39 +0300

    Update styles, optimize landing video

--- app/(home)/components/LandingVideo.tsx
+++ app/(home)/components/LandingVideo.tsx
@@ -1,9 +1,10 @@
 const videoUrl = "https://cdn.xreos.co/usercontent:videos:00d0c1e0-d032-43ba-a6fd-3c022dc5362e.webm"
+//const videoUrl = "/teaser.webm"
 
 const LandingVideo = () => {
     return (
         <>
-            <video className="w-screen h-auto brightness-50 min-h-[38rem] object-cover object-center" autoPlay muted loop poster="/poster.webp" src="/teaser.webm"></video>
+            <video className="w-screen h-auto brightness-50 min-h-[38rem] object-cover object-center" autoPlay muted loop poster="/poster.webp" src={videoUrl}></video>
             <div className="absolute w-full h-full bg-[#770000B2] mix-blend-multiply inset-0"></div>
         </>
     );
