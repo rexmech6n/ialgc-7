4a563c4d5a729910c36e78161534f7952237bbba
Cem Baykara <cm0bykr@gmail.com>
2023-10-31 18:00:37 +0300

    Update navbar styles, add application link to landing page

--- app/(home)/components/LandingContent.tsx
+++ app/(home)/components/LandingContent.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const LandingContent = () => {
     return (
         <div className="absolute inset-0 max-w-screen-2xl h-full mx-auto px-3 mt-5 xsm:px-10 xsm:mt-0 flex flex-col justify-center items-start">
@@ -7,10 +9,10 @@ const LandingContent = () => {
             <p style={{fontSize: "clamp(12px, 2dvw, 20px)", textWrap: "balance"} as object} className="my-7 font-medium max-w-2xl">
                 İzmir’e konferans kültürü kazandıran, kalitesinden asla taviz vermeyen, Türkiye’nin en köklü ve iyi konferanslarından biri olan İzmir Atatürk Lisesi Gençlik Çalıştayı tekrardan sizlerle!
             </p>
-            <button className="flex justify-between items-center relative bg-white rounded-[100px] border-[1.5px] border-[#9A0000CC]">
+            <Link href={'/apply'} className="flex justify-between items-center relative bg-white rounded-[100px] border-[1.5px] border-[#9A0000CC]">
                 <span style={{fontSize: "clamp(12px, 2dvw, 18px)"}} className="inline-block bg-gradient-to-r p-2 pl-5 pr-3 text-transparent bg-clip-text font-semibold from-[#000] from-40% to-[#F00] to-100%">Başvuru Yap</span>
                 <span className="flex justify-center items-center aspect-square w-10 md:w-12 rounded-[100px] bg-gradient-to-l from-[#F00] from-0% to-[#260D0D] to-134% text-2xl font-bold">{">"}</span>
-            </button>
+            </Link>
         </div>
     )
 }
--- components/Navbar/NavbarLink.tsx
+++ components/Navbar/NavbarLink.tsx
@@ -16,7 +16,7 @@ const NavbarLink = ({children, path, icon}: NavbarLinkProps) => {
     const active = pathname === path
 
     return (
-        <Link href={path} className={`flex items-center rounded-full transition-colors duration-200 text-[#6B1710] ease-linear py-1.5 px-1.5 mx-1.5 sm:py-2 sm:px-3 sm:mx-2 text-base ${getConditionalStyles("font-semibold bg-[#DE55557F] [&>div>svg]:fill-[#6B1710] text-xs sm:text-base delay-200", active)}`}>
+        <Link href={path} className={`flex items-center rounded-full transition-colors duration-200 text-[#6B1710] ease-linear py-1.5 px-2 mx-1 sm:py-2 sm:px-3 sm:mx-2 text-base ${getConditionalStyles("font-semibold bg-[#DE55557F] [&>div>svg]:fill-[#6B1710] text-xs sm:text-base delay-200", active)}`}>
             <div className={`mr-1 sm:mr-2 [&>svg]:w-6 [&>svg]:h-6 [&>svg]:sm:w-8 [&>svg]:sm:h-8 [&>svg]:transition-colors [&>svg]:duration-200 [&>svg]:ease-linear ${getConditionalStyles("[&>svg]:delay-200", active)}`}>
                 {icon}
             </div>
