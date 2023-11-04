9b6732bdcd4cedb132276413f3b56bf1ed8bc7a1
Cem Baykara <cm0bykr@gmail.com>
2023-11-04 18:15:24 +0300

    bug fix

--- hooks/useSignInWithEmailLink.ts
+++ hooks/useSignInWithEmailLink.ts
@@ -43,7 +43,7 @@ export default function useSignInWithEmailLink() {
         const email = ((new FormData(e.target as HTMLFormElement)).get("email") as string).trim()
 
         const actionCodeSettings = {
-            url: `https://ialgc23.vercel.app/${pathname}`,
+            url: `https://ialgc23.org/${pathname}`,
             handleCodeInApp: true
         }
 
