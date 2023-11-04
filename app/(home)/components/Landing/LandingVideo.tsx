c01db03f335c75e1db144d5e2a0c2347e74fa73d
Cem Baykara <cm0bykr@gmail.com>
2023-11-04 13:20:37 +0300

    bug fix

--- app/(home)/components/Landing/LandingVideo.tsx
+++ app/(home)/components/Landing/LandingVideo.tsx
@@ -1,11 +1,20 @@
+import Script from "next/script";
+
 const videoUrl = "https://cdn.xreos.co/usercontentvideoccb1b626-b002-4249-a013-ffa742b3dd3d.mp4"
 //const videoUrl = "/teaser.mp4"
 
 const LandingVideo = () => {
     return (
-        <>  
+        <>
             <video className="w-screen brightness-90 md:brightness-50 h-[60dvh] min-h-[30rem] md:h-[70dvh] max-h-[40rem] lg:max-h-none lg:min-h-[45rem] object-cover object-center" autoPlay={true} muted={true} loop={true} poster="/poster.webp" src={videoUrl}></video>
             <div className="absolute w-full h-full bg-[#770000B2] mix-blend-multiply inset-0"></div>
+            <Script id="video-script">
+                {`
+                const video = document.querySelector("video")
+                video.play()
+                `
+                }
+            </Script>
         </>
     );
 };
