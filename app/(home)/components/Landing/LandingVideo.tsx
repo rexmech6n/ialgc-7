dde1598ca34418b7c94e3e1b5a34a687f8401e9e
Cem Baykara <cm0bykr@gmail.com>
2023-11-04 13:13:59 +0300

    bug fix

--- app/(home)/components/Landing/LandingVideo.tsx
+++ app/(home)/components/Landing/LandingVideo.tsx
@@ -4,7 +4,7 @@ const videoUrl = "https://cdn.xreos.co/usercontentvideoccb1b626-b002-4249-a013-f
 const LandingVideo = () => {
     return (
         <>  
-            <video className="w-screen brightness-90 md:brightness-50 h-[60dvh] min-h-[30rem] md:h-[70dvh] max-h-[40rem] lg:max-h-none lg:min-h-[45rem] object-cover object-center" autoPlay muted loop poster="/poster.webp" src={videoUrl}></video>
+            <video className="w-screen brightness-90 md:brightness-50 h-[60dvh] min-h-[30rem] md:h-[70dvh] max-h-[40rem] lg:max-h-none lg:min-h-[45rem] object-cover object-center" autoPlay={true} muted={true} loop={true} poster="/poster.webp" src={videoUrl}></video>
             <div className="absolute w-full h-full bg-[#770000B2] mix-blend-multiply inset-0"></div>
         </>
     );
