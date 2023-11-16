664a336cf359d10bf4c3bc9f18e46f331f5fa24d
Cem Baykara <cm0bykr@gmail.com>
2023-11-16 22:56:23 +0300

    Update caching behaviour

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,7 @@ import Landing from "@/app/(home)/components/Landing";
 import Memories from "@/app/(home)/components/Memories";
 import Counter from "@/app/(home)/components/Counter";
 
-export const dynamic = "force-dynamic"
+export const revalidate = 60
 
 export default function Home() {
     return (
