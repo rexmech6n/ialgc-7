573eff8dbed55bfa001cfe4f43d67d0f0d09223c
Cem Baykara <cm0bykr@gmail.com>
2023-11-16 13:54:24 +0300

    Add /account page, remove /ticket page, update styles

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,5 @@
 import UnderDevelopment from "@/components/UnderDevelopment";
 
-export default function TicketPage() {
+export default function AccountPage() {
     return (<UnderDevelopment />)
 }
