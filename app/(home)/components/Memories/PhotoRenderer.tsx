e84277dfb04d05fb88962bb8f4ca9277cd6db025
Cem Baykara <cm0bykr@gmail.com>
2023-11-16 15:18:55 +0300

    Update easter egg

--- app/(home)/components/Memories/PhotoRenderer.tsx
+++ app/(home)/components/Memories/PhotoRenderer.tsx
@@ -6,15 +6,19 @@ const PhotoRenderer = () => {
 
     const [count, setCount] = useState(0)
 
-    const handleClick = () => setCount(count + 1)
+    const handleClick = () => {
+        console.log("click")
+        setCount(count + 1)
+    }
 
     const displayEasterEgg = count >= 31
 
     return (
         <div className="flex flex-wrap justify-evenly xsm:justify-center">
             {[1, 2, 3, 4].map((i) => (
-                <div onClick={i === 3 ? handleClick : undefined} className={`shrink-0 basis-[45%] md:basis-[30%] lg:basis-[23%] relative w-96 ${!displayEasterEgg ? "h-32" : "h-48"} xsm:h-48 md:h-64 xl:h-[20rem] mx-0 sm:mx-2 rounded-xl overflow-hidden`} key={i}>
-                    <Image className="object-contain" src={displayEasterEgg ? "/memories/easterEgg.webp" : `/memories/image${i}.webp`} alt={"memory"} fill />
+                <div className={`shrink-0 basis-[45%] md:basis-[30%] lg:basis-[23%] relative w-96 ${!displayEasterEgg ? "h-32" : "h-48"} xsm:h-48 md:h-64 xl:h-[20rem] mx-0 sm:mx-2 rounded-xl overflow-hidden`} key={i}>
+                    {i === 2 && <div className="absolute top-[30%] left-[15%] w-[22%] h-[30%] z-30 bg-transparent" onClick={handleClick}></div>}
+                    <Image className="object-contain z-20" src={displayEasterEgg ? "/memories/easterEgg.webp" : `/memories/image${i}.webp`} alt={"memory"} fill />
                 </div>
             ))}
             {[5, 6, 7].map(i => (
