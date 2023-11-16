afe88aac896f817c19d04be70e470729559d2a7c
Cem Baykara <cm0bykr@gmail.com>
2023-11-16 16:18:02 +0300

    Update flappy bird assets

--- app/game/page.jsx
+++ app/game/page.jsx
@@ -7,7 +7,7 @@ export default function GamePage() {
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
         const img = new Image();
-        img.src = "https://gcdnb.pbrd.co/images/zoENrZf3pdM9.png?o=1";
+        img.src = "https://gcdnb.pbrd.co/images/tClr6OcGk52b.png?o=1";
 
 // general settings
         let gamePlaying = false;
