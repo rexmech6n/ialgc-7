c2e67fa25049adcdbc3c308d9e925af3f8575ff3
Cem Baykara <cm0bykr@gmail.com>
2024-04-20 13:00:48 +0300

    Update metadata

---
+++ app/game/layout.tsx
@@ -0,0 +1,15 @@
+import {ReactNode} from "react"
+import {Metadata} from "next"
+
+type GameLayoutProps = {
+    children: ReactNode
+}
+
+export const metadata: Metadata = {
+    title: "Nayki Bird | VII. İALGÇ",
+    description: "Konferansımızın resmi oyunu olan Nayki Bird'ü deneyin!",
+}
+
+export default function GameLayout({ children } : GameLayoutProps) {
+    return children
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import {MetadataRoute} from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://ialgc.org",
+            lastModified: new Date(),
+            priority: 1
+        },
+        {
+            url: "https://ialgc.org/apply",
+            lastModified: new Date(),
+            priority: 0.8
+        },
+        {
+            url: "https://ialgc.org/committees",
+            lastModified: new Date(),
+            priority: 0.5
+        },
+        {
+            url: "https://ialgc.org/game",
+            lastModified: new Date(),
+            priority: 0.2
+        }
+    ]
+}
