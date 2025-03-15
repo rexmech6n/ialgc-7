c894b850363ddc82fc5025ea4a8dfbb30baa00ca
rexmech6n <cm0bykr@gmail.com>
2025-03-15 17:43:53 +0300

    Update video url

--- app/(home)/components/Landing/LandingContent.tsx
+++ app/(home)/components/Landing/LandingContent.tsx
@@ -35,7 +35,7 @@ const LandingContent = () => {
                 </Link>
             </AnimationContainer>
 
-            <Link className="flex justify-center items-center text-center max-w-[180px] md:max-w-[230px] box-content px-3 py-2 rounded-2xl absolute bottom-9 xsm:bottom-6 right-2.5 bg-gradient-to-r from-[#FF8A00] to-[#F00]" href="https://www.instagram.com/reel/CyliSDRreJr/?igshid=MTc4MmM1YmI2Ng==" target="_blank">
+            <Link className="flex justify-center items-center text-center max-w-[180px] md:max-w-[230px] box-content px-3 py-2 rounded-2xl absolute bottom-9 xsm:bottom-6 right-2.5 bg-gradient-to-r from-[#FF8A00] to-[#F00]" href="https://www.instagram.com/reel/DHOPHino-UD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" target="_blank">
                 <div className="bg-white rounded-full p-2 [&>svg]:w-[20px] [&>svg]:h-[20px] xsm:[&>svg]:w-[28px] xsm:[&>svg]:h-[28px]">
                     <svg xmlns="http://www.w3.org/2000/svg" width={28} height={28} viewBox="0 0 48 41" fill="none">
                         <path d="M17.8801 27.7056L33.8425 18.1336L17.8801 8.50843V27.7056ZM14.7779 41V36.7458H5.302C3.83361 36.7458 2.58284 36.2588 1.54971 35.2847C0.516569 34.3107 0 33.1315 0 31.7471V5.05188C0 3.65285 0.516569 2.46112 1.54971 1.47669C2.58284 0.492229 3.83361 0 5.302 0H42.6416C44.1255 0 45.3896 0.492229 46.4337 1.47669C47.4779 2.46112 48 3.65285 48 5.05188V31.7471C48 33.1315 47.4779 34.3107 46.4337 35.2847C45.3896 36.2588 44.1255 36.7458 42.6416 36.7458H33.2221V41H14.7779ZM5.302 31.7471H42.6416V5.05188H5.302V31.7471Z" fill="url(#paint0_linear_212_65)"/>
